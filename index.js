151e84e48d2036362a4448bf88f7081983dd358a
Cervinschi Mario Andrei <mario.cervinschi@bmwtechworks.ro>
2025-12-09 13:08:44 +0200

    .

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function navBurgerClick() {
-  const elem = document.getElementById("nav-links");
-  if (elem.style.display === "none" || elem.style.display === "") {
-    elem.style.display = "flex";
-  } else {
-    elem.style.display = "none";
-  }
-}
-
-document.getElementById("nav-burger").addEventListener("click", navBurgerClick);
+document.getElementById("nav-burger").addEventListener("click", function () {
+  document.getElementById("nav-links").classList.toggle("show");
+});
