6c3c666393cd1e292b492da8acf9a73597865df2
Cervinschi Mario Andrei <mario.cervinschi@bmwtechworks.ro>
2026-04-01 16:02:46 +0300

    some more tests/

--- forms-homework-3/frontend/src/app/pages/jobs-table/jobs-table.component.spec.ts
+++ forms-homework-3/frontend/src/app/pages/jobs-table/jobs-table.component.spec.ts
@@ -8,12 +8,18 @@ import { PaginatedResponse } from '../../shared/models/pagination.model';
 import { Job } from '../../shared/models/job.model';
 import { WrapperComponent } from '../../shared/components/wrapper/wrapper.component';
 import { MatProgressBar } from '@angular/material/progress-bar';
-import { MatFormField } from '@angular/material/form-field';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatIcon } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 class MockJobsService implements Partial<JobsService> {
   private mockData: PaginatedResponse<Job> = {
     data: [],
-    pagination: { totalItems: 0, itemsPerPage: 10, totalPages: 0, currentPage: 1 },
+    pagination: { totalItems: 10, itemsPerPage: 3, totalPages: 4, currentPage: 1 },
   };
 
   getAll(params: any) {
@@ -29,18 +35,65 @@ describe('JobsTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [JobsTableComponent],
+      imports: [
+        JobsTableComponent,
+        WrapperComponent,
+        MatProgressBar,
+        MatFormField,
+        MatLabel,
+        MatTableModule,
+        MatIcon,
+        MatPaginatorModule,
+        MatInputModule,
+        FormsModule,
+        DatePipe,
+        AsyncPipe,
+      ],
       providers: [{ provide: JobsService, useClass: MockJobsService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(JobsTableComponent);
     jobsService = TestBed.inject(JobsService) as unknown as MockJobsService;
+    component = fixture.componentInstance;
   });
 
-  it('TODO: Check getAll is called on component init with no args', () => {});
+  it('TO DO: Check getAll is called on component init with no args', async () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {});
+  });
   it('TODO: response of getAll() emits the paginated response returned by the mock???', () => {});
-  it('TODO: search input field is rendered in DOM', () => {});
+  it('search input field is rendered in DOM', () => {
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('mat-form-field'))).toBeTruthy();
+  });
   it('TODO: onPageChange() calls getAll() with the new page and limit', () => {});
-  it('TODO: an empty search val passes undefined as search (not empty string) to getAll()', () => {});
+  it('an empty search val DOES NOT pass undefined as search (but empty string) to getAll()', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      const input = fixture.debugElement.query(By.css('input'));
+      const element = input.nativeElement;
+
+      expect(element.value).toBe('');
+      expect(element.value).not.toBe(undefined);
+
+      element.value = 'otherVal';
+      element.dispatchEvent(new Event('input'));
+
+      expect(element.value).toBe('otherVal');
+      expect(element.value).not.toBe('');
+      expect(element.value).not.toBe(undefined);
+
+      element.value = '';
+      element.dispatchEvent(new Event('input'));
+
+      expect(element.value).toBe('');
+      expect(element.value).not.toBe(undefined);
+    });
+  });
   it('TODO: getAll() is NOT called a second time during init if the search control has not changed', () => {});
 });
