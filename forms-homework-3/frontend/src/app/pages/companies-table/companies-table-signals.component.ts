277abf521f3eac126324026899d8598dc7ecec4d
Cervinschi Mario Andrei <mario.cervinschi@bmwtechworks.ro>
2026-04-03 11:45:47 +0300

    clear search on companies

--- forms-homework-3/frontend/src/app/pages/companies-table/companies-table-signals.component.ts
+++ forms-homework-3/frontend/src/app/pages/companies-table/companies-table-signals.component.ts
@@ -1,26 +1,16 @@
-import { Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { WrapperComponent } from '../../shared/components/wrapper/wrapper.component';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { Company } from '../../shared/models/company.model';
 import { MatTableModule } from '@angular/material/table';
 import { MatIcon } from '@angular/material/icon';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import {
-  BehaviorSubject,
-  combineLatest,
-  debounceTime,
-  distinctUntilChanged,
-  filter,
-  finalize,
-  switchMap,
-  tap,
-} from 'rxjs';
+import { combineLatest, debounceTime, distinctUntilChanged, finalize, switchMap, tap } from 'rxjs';
 import { CompaniesService } from '../../shared/services/company.service';
-import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { AsyncPipe } from '@angular/common';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-companies-table',
@@ -34,6 +24,7 @@ import { AsyncPipe } from '@angular/common';
     MatPaginatorModule,
     MatInputModule,
     FormsModule,
+    MatButtonModule,
   ],
   templateUrl: './companies-table-signals.component.html',
   styleUrl: './companies-table.component.scss',
@@ -44,12 +35,15 @@ export class CompaniesTableSignalsComponent {
   protected readonly pageIndex = signal<number>(0);
   protected readonly itemsPerPage = signal<number>(10);
   protected readonly searchValue = signal<string>('');
+  protected readonly showX = computed(() => {
+    return this.searchValue().length > 0;
+  });
 
   protected readonly displayedColumns: string[] = ['company', 'location', 'website', 'open_jobs'];
 
   private readonly companiesResource$ = combineLatest({
-    page: toObservable(this.pageIndex),
-    limit: toObservable(this.itemsPerPage),
+    page: toObservable(this.pageIndex).pipe(distinctUntilChanged()),
+    limit: toObservable(this.itemsPerPage).pipe(distinctUntilChanged()),
     search: toObservable(this.searchValue).pipe(debounceTime(500), distinctUntilChanged()),
   }).pipe(
     tap(() => this.loadingCompanies.set(true)),
@@ -71,6 +65,14 @@ export class CompaniesTableSignalsComponent {
     () => this.companiesData()?.pagination.totalItems ?? 0,
   );
   protected readonly loadingCompanies = signal<boolean>(false);
+
+  protected readonly isEmpty = computed(() => {
+    return this.companiesAvailable().length === 0;
+  });
+
+  clickX(event: Event) {
+    this.searchValue.set('');
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
