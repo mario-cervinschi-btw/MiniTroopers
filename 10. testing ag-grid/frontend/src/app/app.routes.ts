9b044a9ab3a23748aaabbd0a5e4016d802f8cdc5
Cervinschi Mario Andrei <mario.cervinschi@bmwtechworks.ro>
2026-04-30 16:08:59 +0300

    feat: Test New Project Functionality
    Create new project (10. testing ag-grid) and learn usage of the AG Grid library.

---
+++ 10. testing ag-grid/frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TableComponent } from './table-component/table-component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: TableComponent,
+  },
+];
---
+++ 10. testing ag-grid/frontend/src/app/table-component/table-component.ts
@@ -0,0 +1,105 @@
+import { Component, inject, signal } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import {
+  themeQuartz,
+  type CellValueChangedEvent,
+  type ColDef,
+  type GridReadyEvent,
+  type RowSelectionOptions,
+  type SelectionChangedEvent,
+} from 'ag-grid-community';
+import { delay, finalize, of, take } from 'rxjs';
+import { data$ } from './table.data';
+import { DatePipe } from '@angular/common';
+
+interface IRow {
+  firstName: string;
+  lastName: string;
+  role: string;
+  birthDate: string;
+  gender: string;
+  workEmail: string;
+  hireDate: string;
+  location: string;
+  directManager: string;
+}
+
+@Component({
+  selector: 'app-table-component',
+  imports: [AgGridAngular],
+  providers: [DatePipe],
+  templateUrl: './table-component.html',
+  styleUrl: './table-component.scss',
+})
+export class TableComponent {
+  private readonly datepipe = inject(DatePipe);
+
+  isLoading = signal<boolean>(false);
+  rowSelection: RowSelectionOptions = {
+    mode: 'multiRow',
+    headerCheckbox: false,
+  };
+  theme = themeQuartz;
+
+  rowData = signal<IRow[]>([]);
+
+  defaultColDef: ColDef = {
+    filter: true,
+  };
+
+  colDefs = signal<ColDef[]>([
+    { field: 'firstName', editable: true },
+    { field: 'lastName', editable: true },
+    { field: 'role' },
+    {
+      field: 'birthDate',
+      valueFormatter: (params) => {
+        return this.datepipe.transform(params.value) ?? 'Invalid Date';
+      },
+    },
+    {
+      field: 'gender',
+      valueFormatter: (params) => {
+        const genderEmoji =
+          params.value.toLowerCase() === 'male'
+            ? '👨'
+            : params.value.toLowerCase() === 'female'
+              ? '👩'
+              : 'Unknown gender';
+        return genderEmoji + ' ' + params.value;
+      },
+    },
+    { field: 'workEmail' },
+    {
+      field: 'hireDate',
+      valueFormatter: (params) => {
+        return this.datepipe.transform(params.value) ?? 'Invalid Date';
+      },
+    },
+    { field: 'location' },
+    { field: 'directManager' },
+  ]);
+
+  onGridReady(params: GridReadyEvent) {
+    this.isLoading.set(true);
+    data$
+      .pipe(
+        delay(2000),
+        take(1),
+        finalize(() => this.isLoading.set(false)),
+      )
+      .subscribe({
+        next: (val) => {
+          this.rowData.set(val);
+        },
+      });
+  }
+
+  onCellValueChanged = (event: CellValueChangedEvent) => {
+    console.log(`New Cell Value: ${event.value}`);
+  };
+
+  onSelectionChanged = (event: SelectionChangedEvent) => {
+    console.log(event.selectedNodes);
+  };
+}
